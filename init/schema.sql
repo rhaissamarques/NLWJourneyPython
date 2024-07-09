5453ddf3c10bbaf4b10820f6f0fa8858914e0f7e
Rhaissa Marques <rhaissa.marques@gmail.com>
2024-07-09 17:08:35 -0300

    feat: Implementing links repository with test

--- init/schema.sql
+++ init/schema.sql
@@ -18,6 +18,7 @@ CREATE TABLE IF NOT EXISTS 'emails_to_invite' (
 CREATE TABLE IF NOT EXISTS 'links' (
   id TEXT PRIMARY KEY,
   trip_id TEXT,
-  links TEXT NOT NULL,
+  link TEXT NOT NULL,
+  title TEXT NOT NULL,
   FOREIGN KEY (trip_id) REFERENCES trips(id)
 );
